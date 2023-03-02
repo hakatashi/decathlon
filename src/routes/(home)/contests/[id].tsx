87ababa727baa154a12c8a5a6d7c01169655a8f9
Koki Takahashi <hakatasiloving@gmail.com>
2023-03-02 22:40:14 +0000

    Stylize athlon header

--- src/routes/(home)/contests/[id].tsx
+++ src/routes/(home)/contests/[id].tsx
@@ -1,9 +1,12 @@
 import {Typography, Container} from '@suid/material';
 import {collection, CollectionReference, doc, DocumentReference, getFirestore, query, where} from 'firebase/firestore';
 import {useFirebaseApp, useFirestore} from 'solid-firebase';
+import {Show} from 'solid-js';
 import {useParams} from 'solid-start';
+import styles from './[id].module.css';
 import Collection from '~/components/Collection';
 import Doc from '~/components/Doc';
+import {athlonNames} from '~/lib/const';
 import type {Game, Contest} from '~/lib/schema';
 
 const Home = () => {
@@ -21,7 +24,7 @@ const Home = () => {
 
 	return (
 		<main>
-			<Container maxWidth="xl">
+			<div class={styles.athlonHeader}>
 				<Doc data={contestData}>
 					{(contest) => (
 						<Typography variant="h1">
@@ -29,6 +32,13 @@ const Home = () => {
 						</Typography>
 					)}
 				</Doc>
+				<Show when={athlonNames.get(gamesData?.data?.length)} keyed>
+					{(athlonName) => (
+						<Typography variant="h3" component="h2">{athlonName}</Typography>
+					)}
+				</Show>
+			</div>
+			<Container maxWidth="xl">
 				<ol>
 					<Collection data={gamesData}>
 						{(game) => {
--- src/routes/(home)/settings.tsx
+++ src/routes/(home)/settings.tsx
@@ -119,7 +119,7 @@ const Home = () => {
 								Submit
 							</Button>
 						</Stack>
-					 )}
+					)}
 				</Doc>
 			</Container>
 		</main>
