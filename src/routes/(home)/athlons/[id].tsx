63cf64f832b7634c792279586bd9943088d3f91d
Koki Takahashi <hakatasiloving@gmail.com>
2023-03-04 08:57:10 +0900

    Provide athlon data as context

--- src/routes/(home)/athlons/[id].tsx
+++ src/routes/(home)/athlons/[id].tsx
@@ -1,82 +1,25 @@
-import {Typography, Container, List, ListItem, ListItemAvatar, Avatar, ListItemText, Box} from '@suid/material';
-import {collection, CollectionReference, doc, DocumentReference, getFirestore, orderBy, query, where} from 'firebase/firestore';
+import {doc, DocumentReference, getFirestore} from 'firebase/firestore';
 import {useFirebaseApp, useFirestore} from 'solid-firebase';
-import {Show} from 'solid-js';
-import {A, useParams} from 'solid-start';
-import styles from './[id].module.css';
-import Collection from '~/components/Collection';
-import Doc from '~/components/Doc';
-import {athlonNames} from '~/lib/const';
-import type {Game, Athlon} from '~/lib/schema';
+import {createContext, useContext} from 'solid-js';
+import {Outlet, useParams} from 'solid-start';
+import type {Athlon, UseFireStoreReturn} from '~/lib/schema';
 
-const Home = () => {
+const AthlonContext = createContext<UseFireStoreReturn<Athlon | null | undefined>>();
+
+const AthlonLayout = () => {
 	const param = useParams();
 	const app = useFirebaseApp();
 	const db = getFirestore(app);
 	const athlonRef = doc(db, 'athlons', param.id) as DocumentReference<Athlon>;
 	const athlonData = useFirestore(athlonRef);
-	const gamesData = useFirestore(
-		query(
-			collection(db, 'games') as CollectionReference<Game>,
-			where('athlon', '==', athlonRef),
-			orderBy('order'),
-		),
-	);
 
 	return (
-		<main class={styles.contest}>
-			<div class={styles.athlonHeader}>
-				<Doc data={athlonData}>
-					{(athlon) => (
-						<Typography variant="h1">
-							{athlon.name}
-						</Typography>
-					)}
-				</Doc>
-				<Show when={athlonNames.get(gamesData?.data?.length)} keyed>
-					{(athlonName) => (
-						<Typography variant="h3" component="h2">{athlonName}</Typography>
-					)}
-				</Show>
-			</div>
-			<Container maxWidth="sm">
-				<Typography
-					component="h2"
-					textAlign="center"
-					letterSpacing={30}
-					fontSize={20}
-					fontWeight="bold"
-					mt={5}
-				>
-					競技一覧
-				</Typography>
-				<List sx={{bgcolor: 'background.paper'}}>
-					<Collection data={gamesData}>
-						{(game, index) => {
-							const ruleData = useFirestore(game.rule);
-							const hasScore = ruleData?.data?.users?.some((user) => user.id === 'ZnYLOVTDhWVxlKt2vxhmCDmvqrJ3');
-
-							return (
-								<ListItem component={A} href={`/athlons/${param.id}/${game.rule.id}`}>
-									<ListItemAvatar>
-										<Avatar>
-											{index() + 1}
-										</Avatar>
-									</ListItemAvatar>
-									<Doc data={ruleData}>
-										{(rule) => (
-											<ListItemText primary={rule.name} secondary={`${game.maxPoint * (game.weight ?? 1)}点`}/>
-										)}
-									</Doc>
-								</ListItem>
-							);
-						}}
-					</Collection>
-				</List>
-			</Container>
-		</main>
+		<AthlonContext.Provider value={athlonData}>
+			<Outlet/>
+		</AthlonContext.Provider>
 	);
 };
 
-export default Home;
+export const useAthlon = () => useContext(AthlonContext);
+export default AthlonLayout;
 
---
+++ src/routes/(home)/athlons/[id]/index.tsx
@@ -0,0 +1,82 @@
+import {Typography, Container, List, ListItem, ListItemAvatar, Avatar, ListItemText, Box} from '@suid/material';
+import {collection, CollectionReference, doc, getFirestore, orderBy, query, where} from 'firebase/firestore';
+import {useFirebaseApp, useFirestore} from 'solid-firebase';
+import {Show} from 'solid-js';
+import {A, useParams} from 'solid-start';
+import {useAthlon} from '../[id]';
+import styles from './index.module.css';
+import Collection from '~/components/Collection';
+import Doc from '~/components/Doc';
+import {athlonNames} from '~/lib/const';
+import type {Game} from '~/lib/schema';
+
+const Home = () => {
+	const param = useParams();
+	const athlonData = useAthlon();
+	const app = useFirebaseApp();
+	const db = getFirestore(app);
+	const gamesData = useFirestore(
+		query(
+			collection(db, 'games') as CollectionReference<Game>,
+			where('athlon', '==', doc(db, 'athlons', param.id)),
+			orderBy('order'),
+		),
+	);
+
+	return (
+		<main class={styles.contest}>
+			<div class={styles.athlonHeader}>
+				<Doc data={athlonData}>
+					{(athlon) => (
+						<Typography variant="h1">
+							{athlon.name}
+						</Typography>
+					)}
+				</Doc>
+				<Show when={athlonNames.get(gamesData?.data?.length)} keyed>
+					{(athlonName) => (
+						<Typography variant="h3" component="h2">{athlonName}</Typography>
+					)}
+				</Show>
+			</div>
+			<Container maxWidth="sm">
+				<Typography
+					component="h2"
+					textAlign="center"
+					letterSpacing={30}
+					fontSize={20}
+					fontWeight="bold"
+					mt={5}
+				>
+					競技一覧
+				</Typography>
+				<List sx={{bgcolor: 'background.paper'}}>
+					<Collection data={gamesData}>
+						{(game, index) => {
+							const ruleData = useFirestore(game.rule);
+							const hasScore = ruleData?.data?.users?.some((user) => user.id === 'ZnYLOVTDhWVxlKt2vxhmCDmvqrJ3');
+
+							return (
+								<ListItem component={A} href={`/athlons/${param.id}/${game.rule.id}`}>
+									<ListItemAvatar>
+										<Avatar>
+											{index() + 1}
+										</Avatar>
+									</ListItemAvatar>
+									<Doc data={ruleData}>
+										{(rule) => (
+											<ListItemText primary={rule.name} secondary={`${game.maxPoint * (game.weight ?? 1)}点`}/>
+										)}
+									</Doc>
+								</ListItem>
+							);
+						}}
+					</Collection>
+				</List>
+			</Container>
+		</main>
+	);
+};
+
+export default Home;
+
