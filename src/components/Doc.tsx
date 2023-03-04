778ecfcb7bb0718e103b15d726366c0f0d06497d
Koki Takahashi <hakatasiloving@gmail.com>
2023-03-04 09:09:53 +0900

    Add game page

--- src/components/Doc.tsx
+++ src/components/Doc.tsx
@@ -3,7 +3,7 @@ import {JSX, Match, Switch} from 'solid-js';
 import {UseFireStoreReturn} from '~/lib/schema';
 
 interface Props<T extends DocumentData> {
-	data: UseFireStoreReturn<T | null | undefined> | null,
+	data: UseFireStoreReturn<T | null | undefined> | null | undefined,
 	children: (item: T) => JSX.Element,
 }
 
