cbf0a09ff164ec993edaba009aa1f4db5f4c13c4
Koki Takahashi <hakatasiloving@gmail.com>
2023-03-04 14:39:37 +0900

    Implement Footer

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -1,3 +1,4 @@
+import {Divider, Link, Stack, Typography} from '@suid/material';
 import {Outlet} from 'solid-start';
 import Header from '~/components/Header';
 
@@ -5,6 +6,25 @@ const RootLayout = () => (
 	<div>
 		<Header/>
 		<Outlet/>
+		<Divider sx={{m: 3}}/>
+		<Stack
+			direction="row"
+			spacing={2}
+			justifyContent="center"
+		>
+			<Typography variant="body2">decathlon by @hakatashi</Typography>
+			<Divider orientation="vertical" flexItem/>
+			<Typography variant="body2">
+				<Link
+					target="_blank"
+					rel="noopener noreferrer"
+					underline="hover"
+					href="https://github.com/hakatashi/decathlon"
+				>
+					GitHub
+				</Link>
+			</Typography>
+		</Stack>
 	</div>
 );
 
