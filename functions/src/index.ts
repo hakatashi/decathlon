348f7b77debaf5e723158e0a6a5ce346cddac106
Koki Takahashi <hakatasiloving@gmail.com>
2023-03-05 15:49:05 +0900

    Split ranking calculation to divided file

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,10 @@
-/* eslint-disable array-plural/array-plural */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 
 import {initializeApp} from 'firebase-admin/app';
 import {DocumentReference, getFirestore} from 'firebase-admin/firestore';
 import type {CollectionReference, CollectionGroup} from 'firebase-admin/firestore';
 import {auth, firestore} from 'firebase-functions';
-import {orderBy, sortBy, sum} from 'lodash';
 import type {Athlon, Game, Score} from '../../src/lib/schema';
-import {calculateScore} from './scores';
+import {calculateRanking} from './scores';
 
 initializeApp();
 const db = getFirestore();
@@ -37,14 +33,8 @@ export const onUserCreated = auth.user().onCreate(async (user) => {
 
 export const onScoreChanged = firestore
 	.document('games/{gameId}/scores/{userId}')
-	.onWrite(async (change, context) => {
+	.onWrite(async (_change, context) => {
 		const changedGameId = context.params.gameId;
-
-		interface ScoreEntry extends Score {
-			rank: number,
-			point: number,
-			userId: string,
-		}
 
 		const gamesRef = db.collection('games') as CollectionReference<Game>;
 		const gameDoc = await gamesRef.doc(changedGameId).get();
@@ -57,136 +47,7 @@ export const onScoreChanged = firestore
 			.where('athlon', '==', athlon)
 			.get();
 
-		type ScoreUserId = Score & { userId: string };
-		const scoresMap = new Map<string, ScoreUserId[]>();
-		for (const score of scoreDocs.docs) {
-			const gameId = score.ref.parent.parent?.id!;
-			if (!scoresMap.has(gameId)) {
-				scoresMap.set(gameId, []);
-			}
-			scoresMap.get(gameId)!.push({...score.data(), userId: score.id});
-		}
-
-		const gamesMap = new Map<string, Game>();
-		for (const game of gameDocs.docs) {
-			gamesMap.set(game.id, game.data());
-		}
-
-		const usersSet = new Set<string>();
-
-		const gameScores: [string, ScoreEntry[]][] = [];
-
-		for (const game of gameDocs.docs) {
-			const gameId = game.id;
-			const scores = scoresMap.get(gameId) ?? [];
-			const sortedScores = orderBy(scores, ['rawScore', 'tiebreakScore'], ['desc', 'desc']);
-
-			for (const score of scores) {
-				usersSet.add(score.userId);
-			}
-
-			let previousRawScore: number | null = null;
-			let previousTiebreakScore: number | null = null;
-			let previousRank = 0;
-			const sortedScoreEntries = sortedScores.map((score, index) => {
-				const scoreEntry = {
-					...score,
-					rank: index,
-					point: 0,
-					userId: score.userId,
-				} as ScoreEntry;
-
-				if (score.rawScore === previousRawScore && score.tiebreakScore === previousTiebreakScore) {
-					scoreEntry.rank = previousRank;
-				} else {
-					previousRank = index;
-				}
-
-				previousRawScore = score.rawScore;
-				previousTiebreakScore = score.tiebreakScore;
-
-				scoreEntry.point = calculateScore(
-					scoreEntry.rawScore,
-					scoreEntry.rank,
-					game.data().maxPoint,
-					game.data().scoreConfiguration,
-				);
-
-				return scoreEntry;
-			});
-
-			gameScores.push([gameId, sortedScoreEntries]);
-		}
-
-		interface UserScoreEntry {
-			userId: string,
-			point: number,
-			games: {
-				gameId: string,
-				hasScore: boolean,
-				point: number,
-				rawScore: number,
-				tiebreakScore: number,
-				rank: number | null,
-			}[],
-		}
-
-		const userScoreEntries = Array.from(usersSet).map((userId) => {
-			const games = gameScores.map(([gameId, scores]) => {
-				const score: ScoreEntry | undefined = scores.find((s) => s.userId === userId);
-				if (!score) {
-					return {
-						gameId,
-						hasScore: false,
-						point: 0,
-						rawScore: 0,
-						tiebreakScore: 0,
-						rank: null,
-					};
-				}
-				return {
-					gameId,
-					hasScore: true,
-					point: score.point,
-					rawScore: score.rawScore,
-					tiebreakScore: score.tiebreakScore,
-					rank: score.rank,
-				};
-			});
-
-			const pointSum = sum(games.map(({gameId, point}) => {
-				const game = gamesMap.get(gameId);
-				return point * (game?.weight ?? 1);
-			}));
-
-			return {
-				userId,
-				point: pointSum,
-				games,
-			} as UserScoreEntry;
-		});
-
-		interface SortedUserScoreEntry extends UserScoreEntry {
-			rank: number,
-		}
-
-		let previousPoint: number | null = null;
-		let previousRank = 0;
-		const ranking = sortBy(userScoreEntries, 'point').reverse().map((userScoreEntry, index) => {
-			let rank = index;
-			if (userScoreEntry.point === previousPoint) {
-				rank = previousRank;
-			} else {
-				previousRank = index;
-			}
-
-			previousPoint = userScoreEntry.point;
-
-			return {
-				...userScoreEntry,
-				rank,
-			} as SortedUserScoreEntry;
-		});
+		const ranking = calculateRanking(gameDocs, scoreDocs);
 
 		await athlon.update({
 			ranking,
--- functions/src/scores.ts
+++ functions/src/scores.ts
@@ -1,4 +1,10 @@
-import {ScoreConfiguration} from '../../src/lib/schema';
+/* eslint-disable array-plural/array-plural */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+
+import type {QuerySnapshot} from 'firebase-admin/firestore';
+import {orderBy, sortBy, sum} from 'lodash';
+import type {Game, Score, ScoreConfiguration} from '../../src/lib/schema';
 
 // eslint-disable-next-line import/prefer-default-export
 export const calculateScore = (rawScore: number, rank: number, maxPoint: number, configuration: ScoreConfiguration) => {
@@ -13,4 +19,146 @@ export const calculateScore = (rawScore: number, rank: number, maxPoint: number,
 	const rankPoint = maxRankPoint * configuration.rankWeight / (rank + configuration.rankWeight);
 
 	return scorePoint + rankPoint;
+};
+
+interface ScoreEntry extends Score {
+	rank: number,
+	point: number,
+	userId: string,
+}
+
+export const calculateRanking = (gameDocs: QuerySnapshot<Game>, scoreDocs: QuerySnapshot<Score>) => {
+	type ScoreUserId = Score & { userId: string };
+
+	const scoresMap = new Map<string, ScoreUserId[]>();
+	for (const score of scoreDocs.docs) {
+		const gameId = score.ref.parent.parent?.id!;
+		if (!scoresMap.has(gameId)) {
+			scoresMap.set(gameId, []);
+		}
+		scoresMap.get(gameId)!.push({...score.data(), userId: score.id});
+	}
+
+	const gamesMap = new Map<string, Game>();
+	for (const game of gameDocs.docs) {
+		gamesMap.set(game.id, game.data());
+	}
+
+	const usersSet = new Set<string>();
+
+	const gameScores: [string, ScoreEntry[]][] = [];
+
+	for (const game of gameDocs.docs) {
+		const gameId = game.id;
+		const scores = scoresMap.get(gameId) ?? [];
+		const sortedScores = orderBy(scores, ['rawScore', 'tiebreakScore'], ['desc', 'desc']);
+
+		for (const score of scores) {
+			usersSet.add(score.userId);
+		}
+
+		let previousRawScore: number | null = null;
+		let previousTiebreakScore: number | null = null;
+		let previousRank = 0;
+		const sortedScoreEntries = sortedScores.map((score, index) => {
+			const scoreEntry = {
+				...score,
+				rank: index,
+				point: 0,
+				userId: score.userId,
+			} as ScoreEntry;
+
+			if (score.rawScore === previousRawScore && score.tiebreakScore === previousTiebreakScore) {
+				scoreEntry.rank = previousRank;
+			} else {
+				previousRank = index;
+			}
+
+			previousRawScore = score.rawScore;
+			previousTiebreakScore = score.tiebreakScore;
+
+			scoreEntry.point = calculateScore(
+				scoreEntry.rawScore,
+				scoreEntry.rank,
+				game.data().maxPoint,
+				game.data().scoreConfiguration,
+			);
+
+			return scoreEntry;
+		});
+
+		gameScores.push([gameId, sortedScoreEntries]);
+	}
+
+	interface UserScoreEntry {
+		userId: string,
+		point: number,
+		games: {
+			gameId: string,
+			hasScore: boolean,
+			point: number,
+			rawScore: number,
+			tiebreakScore: number,
+			rank: number | null,
+		}[],
+	}
+
+	const userScoreEntries = Array.from(usersSet).map((userId) => {
+		const games = gameScores.map(([gameId, scores]) => {
+			const score: ScoreEntry | undefined = scores.find((s) => s.userId === userId);
+			if (!score) {
+				return {
+					gameId,
+					hasScore: false,
+					point: 0,
+					rawScore: 0,
+					tiebreakScore: 0,
+					rank: null,
+				};
+			}
+			return {
+				gameId,
+				hasScore: true,
+				point: score.point,
+				rawScore: score.rawScore,
+				tiebreakScore: score.tiebreakScore,
+				rank: score.rank,
+			};
+		});
+
+		const pointSum = sum(games.map(({gameId, point}) => {
+			const game = gamesMap.get(gameId);
+			return point * (game?.weight ?? 1);
+		}));
+
+		return {
+			userId,
+			point: pointSum,
+			games,
+		} as UserScoreEntry;
+	});
+
+	interface SortedUserScoreEntry extends UserScoreEntry {
+		rank: number,
+	}
+
+	let previousPoint: number | null = null;
+	let previousRank = 0;
+	const ranking = sortBy(userScoreEntries, 'point').reverse().map((userScoreEntry, index) => {
+		let rank = index;
+		if (userScoreEntry.point === previousPoint) {
+			rank = previousRank;
+		} else {
+			previousRank = index;
+		}
+
+		previousPoint = userScoreEntry.point;
+
+		return {
+			...userScoreEntry,
+			rank,
+		} as SortedUserScoreEntry;
+	});
+
+	return ranking;
 };
